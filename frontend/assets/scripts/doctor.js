cbdfa85a01fc4dc24500895d295470c9da902505
metamodern <shabanovbunya@gmail.com>
2019-10-14 11:54:31 +0300

    front fix all buttons of doctor

--- frontend/assets/scripts/doctor.js
+++ frontend/assets/scripts/doctor.js
@@ -8,13 +8,41 @@ function treatmentSend() {
 
 
 function closeTreatment() {
-  const treatwindow = document.getElementsByClassName('patients_tratment')[0];
+  const treatwindow = document.getElementsByClassName('patients_treatment')[0];
   treatwindow.style.visibility = 'hidden';
 }
 
 
 function openTratment() {
-  const treatwindow = document.getElementsByClassName('patients_tratment')[0];
+  const treatwindow = document.getElementsByClassName('patients_treatment')[0];
   treatwindow.style.visibility = 'visible';
 
+}
+
+
+function toDocSend(){
+  const docdoc = document.getElementsByClassName('another_doc')[0];
+  docdoc.style.visibility = 'hidden';
+  //тут еще функция отправки к другому врачу(отправка формы)
+
+}
+
+function openDocSend() {
+  const docdoc = document.getElementsByClassName('another_doc')[0];
+  docdoc.style.visibility = 'visible';
+
+}
+
+
+
+function showBanWindow() {
+  const ban = document.getElementsByClassName('ban_user')[0];
+  ban.style.visibility = 'visible';
+}
+
+
+function Report() {
+  const ban = document.getElementsByClassName('ban_user')[0];
+  ban.style.visibility = 'hidden';
+  //сюда еще функцию отправки запроса о нарушении
 }
